818420b054089db9cb8d3e22dc9b97688d2d3b54
karnwaltushti <34749539+karnwaltushti@users.noreply.github.com>
2020-02-20 23:47:35 +0530

    Delete javas.js

--- view/javas.js
+++
@@ -1,8 +0,0 @@
-var x="",i,j;
-var obj=JSON.parse(#{data})
-document.getElementById("demo").innerHTML = obj;
-for (i in obj) {
-    x += "<h2>" + obj[i].coursename + "</h2>";
-    x += "<h2>" + obj[i].coursecode + "</h2>";  
-}
-document.getElementById("demo").innerHTML = x;
